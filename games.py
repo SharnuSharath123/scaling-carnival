551d101c55a1b38e0ed2e3d36adf92a036c37f4b
SharnuSharath123 <sharnuburli143@gmail.com>
2024-08-31 21:08:06 +0530

    scaling-carnival

---
+++ games.py
@@ -0,0 +1,8 @@
+# games.py
+def get_games():
+    return ["Ring Toss", "Duck Hunt", "Whack-a-Mole"]
+
+if __name__ == "__main__":
+    games = get_games()
+    for game in games:
+        print(game)
